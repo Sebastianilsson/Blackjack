ef647ca9eaaf75f14d8347aca8fdaee0745290a4
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-11 17:16:05 +0100

    REFACTOR

--- BlackjackTest/Controller/GameControllerTest.cs
+++ BlackjackTest/Controller/GameControllerTest.cs
@@ -11,11 +11,16 @@ namespace BlackjackTest.Controller
 {
     public class GameControllerTest
     {
+        private GameController sut;
+        private Mock<IBlackjackGame> mockBlackjackGame;
+        public GameControllerTest()
+        {
+            mockBlackjackGame = new Mock<IBlackjackGame>();
+            sut = new GameController(mockBlackjackGame.Object);
+        }
         [Fact]
         public void PlayGame_ShouldCallToDealANewHand()
         {
-            var mockBlackjackGame = new Mock<IBlackjackGame>();
-            GameController sut = new GameController(mockBlackjackGame.Object);
             sut.PlayGame();
             mockBlackjackGame.Verify(game => game.DealNewHand(), Times.Once());
         }
