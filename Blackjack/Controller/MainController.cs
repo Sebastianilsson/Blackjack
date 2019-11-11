f8956d5629f040c331723e80d70c923d611fde68
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-11 16:10:15 +0100

    GREEN

--- Blackjack/Controller/MainController.cs
+++ Blackjack/Controller/MainController.cs
@@ -25,7 +25,7 @@ namespace Blackjack.Controller
                     GameView.RenderRules();
                     break;
             }
-            
+            GameView.RenderExitMessage();
             
         }
     }
