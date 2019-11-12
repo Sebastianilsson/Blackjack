40d127686caae4e8f1b9593b45d18ad48c9d9a45
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-12 16:32:08 +0100

    RED

--- Blackjack/View/GameView.cs
+++ Blackjack/View/GameView.cs
@@ -4,8 +4,40 @@ using System.Text;
 
 namespace Blackjack.View
 {
-    public class GameView
+    public class GameView : IGameView
     {
+        public void RenderStartMenu()
+        {
+            throw new NotImplementedException();
+        }
+        public StartMenuAction GetStartMenuAction()
+        {
+            throw new NotImplementedException();
+        }
+        public void RenderRules()
+        {
+            throw new NotImplementedException();
+        }
+        public void RenderExitMessage()
+        {
+            throw new NotImplementedException();
+        }
+        public void RenderPlayersHands()
+        {
+            throw new NotImplementedException();
+        }
+        public void RenderGameActionChoices()
+        {
+            throw new NotImplementedException();
+        }
+        public GameAction GetGameAction()
+        {
+            throw new NotImplementedException();
+        }
+        public void RenderResultOfGame()
+        {
+            throw new NotImplementedException();
+        }
     }
     public enum StartMenuAction
     {
---
+++ BlackjackTest/View/GameViewTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using Blackjack.View;
+using System.IO;
+
+namespace BlackjackTest.View
+{
+    public class GameViewTest
+    {
+        [Fact]
+        public void RenderStartMenu_ShouldRenderTheStartMenuInConsoleWhenCalled()
+        {
+            var output = new StringWriter();
+            Console.SetOut(output);
+            IGameView sut = new GameView();
+            string expected = @"1. Play Game
+2. Show Rules
+3. Exit
+Make a Choice (1-3): ";
+            sut.RenderStartMenu();
+            string actual = output.ToString();
+            Assert.Equal(expected, actual);            
+        }
+    }
+}
