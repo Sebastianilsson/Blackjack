477d4a9581eba1262045bb80a63844eb126a1706
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-12 19:48:24 +0100

    GREEN

--- Blackjack/View/GameView.cs
+++ Blackjack/View/GameView.cs
@@ -30,7 +30,7 @@ namespace Blackjack.View
         }
         public void RenderRules()
         {
-            throw new NotImplementedException();
+            Console.Write("This is a simple game of black. When the game starts both you and the dealer get two open cards each. The goal of the game is to get a score of 21. When the cards are dealt you will get the choose between 'hit' or 'stay'. If you choose 'hit' then you will get another card. If your score gets higher than 21 then you are busted and you lose. If you choose to 'stay' on a score lower than 21 it's the dealers turn to draw cards. The dealer needs to keep taking new cards until he has a scor of at least 17. If you have a score higher than 17 the dealer will continue to take cards until he has a score higher than yours. The dealer wins all ties. Good luck! Press 'enter' to continue: ");
         }
         public void RenderExitMessage()
         {
