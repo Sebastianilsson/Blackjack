c254512b35e0cb75c8b125801a61e2f3093ec7b9
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-12 20:49:53 +0100

    REFACTOR

--- Blackjack/View/GameView.cs
+++ Blackjack/View/GameView.cs
@@ -6,6 +6,8 @@ namespace Blackjack.View
 {
     public class GameView : IGameView
     {
+        internal string rules = "RULES\n\nThis is a simple game of Blackjack. \nWhen the game starts both you and the dealer get two open cards each. \nThe goal of the game is to get a score of 21. \nWhen the cards are dealt you will get the choose between 'hit' or 'stay'. \nIf you choose 'hit' then you will get another card. \nIf your score gets higher than 21 then you are busted and you lose. \nIf you choose to 'stay' on a score lower than 21 it's the dealers turn to draw cards. \nThe dealer needs to keep taking new cards until he has a scor of at least 17. \nIf you have a score higher than 17 the dealer will continue to take cards until he has a score higher than yours. \nThe dealer wins all ties. \n\nGood luck! \n\nPress 'enter' to continue: ";
+        internal string exitMessage = "Bye bye!\nThank you for playing!";
         // If you add actions to the StartMenu remember to update test 
         // RenderStartMenu_ShouldRenderTheStartMenuInConsoleWhenCalled()
         public void RenderStartMenu()
@@ -30,7 +32,7 @@ namespace Blackjack.View
         }
         public void RenderRules()
         {
-            Console.Write("RULES\n\nThis is a simple game of Blackjack. \nWhen the game starts both you and the dealer get two open cards each. \nThe goal of the game is to get a score of 21. \nWhen the cards are dealt you will get the choose between 'hit' or 'stay'. \nIf you choose 'hit' then you will get another card. \nIf your score gets higher than 21 then you are busted and you lose. \nIf you choose to 'stay' on a score lower than 21 it's the dealers turn to draw cards. \nThe dealer needs to keep taking new cards until he has a scor of at least 17. \nIf you have a score higher than 17 the dealer will continue to take cards until he has a score higher than yours. \nThe dealer wins all ties. \n\nGood luck! \n\nPress 'enter' to continue: ");
+            Console.Write(rules);
         }
         public string PressEnterToContinue()
         {
@@ -38,7 +40,7 @@ namespace Blackjack.View
         }
         public void RenderExitMessage()
         {
-            Console.Write("Bye bye!\nThank you for playing!");
+            Console.Write(exitMessage);
         }
         public void RenderPlayersHands()
         {
