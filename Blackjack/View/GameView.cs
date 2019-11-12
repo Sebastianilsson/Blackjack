9b0db8e06e46d1d56b4e5917bf95517a35ee3be7
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-12 21:00:17 +0100

    REFACTOR

--- Blackjack/View/GameView.cs
+++ Blackjack/View/GameView.cs
@@ -8,15 +8,16 @@ namespace Blackjack.View
     {
         internal string rules = "RULES\n\nThis is a simple game of Blackjack. \nWhen the game starts both you and the dealer get two open cards each. \nThe goal of the game is to get a score of 21. \nWhen the cards are dealt you will get the choose between 'hit' or 'stay'. \nIf you choose 'hit' then you will get another card. \nIf your score gets higher than 21 then you are busted and you lose. \nIf you choose to 'stay' on a score lower than 21 it's the dealers turn to draw cards. \nThe dealer needs to keep taking new cards until he has a scor of at least 17. \nIf you have a score higher than 17 the dealer will continue to take cards until he has a score higher than yours. \nThe dealer wins all ties. \n\nGood luck! \n\nPress 'enter' to continue: ";
         internal string exitMessage = "Bye bye!\nThank you for playing!";
+
         // If you add actions to the StartMenu remember to update test 
         // RenderStartMenu_ShouldRenderTheStartMenuInConsoleWhenCalled()
         public void RenderStartMenu()
         {
             Array startMenuActions = typeof(StartMenuAction).GetEnumValues();
             int itemListNumber = 1;
-            foreach (StartMenuAction loginType in startMenuActions)
+            foreach (StartMenuAction action in startMenuActions)
             {
-                Console.WriteLine($"{itemListNumber++}. {loginType}");
+                Console.WriteLine($"{itemListNumber++}. {action}");
             }
             Console.Write("Make a Choice (1-3): ");
         }
@@ -48,7 +49,13 @@ namespace Blackjack.View
         }
         public void RenderGameActionChoices()
         {
-            Console.Write("1. Hit\r\n2. Stay\r\nMake a Choice (1-2): ");
+            Array gameActions = typeof(GameAction).GetEnumValues();
+            int itemListNumber = 1;
+            foreach (GameAction action in gameActions)
+            {
+                Console.WriteLine($"{itemListNumber++}. {action}");
+            }
+            Console.Write("Make a Choice (1-2): ");
         }
         public GameAction GetGameAction()
         {
