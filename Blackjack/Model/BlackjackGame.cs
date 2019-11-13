9717310057ba206ccf7ad689cdc032899761c56b
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-13 06:52:09 +0100

    RED

---
+++ Blackjack/Model/BlackjackGame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack.Model
+{
+    public class BlackjackGame : IBlackjackGame
+    {
+        private IDealer Dealer { get; set; }
+
+        public BlackjackGame(IDealer dealer)
+        {
+            Dealer = dealer;
+        }
+
+        public void DealNewHand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsGameOver()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DealerTakeCards()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlackjackTest/Model/BlackjackGameTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using Blackjack.Model;
+
+namespace BlackjackTest.Model
+{
+    public class BlackjackGameTest
+    {
+        [Fact]
+        public void DealNewHand_ShouldMakeACallToDealerToGetANewDeck()
+        {
+            var mockDealer = new Mock<IDealer>();
+            IBlackjackGame sut = new BlackjackGame(mockDealer.Object);
+            sut.DealNewHand();
+            mockDealer.Verify(dealer => dealer.GetNewDeck(), Times.Once());
+
+        }
+    }
+}
