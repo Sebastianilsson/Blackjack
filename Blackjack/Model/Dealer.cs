a1b9edfdb889cb6390f0e5187f0e2e4d6ecf969c
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-14 17:50:36 +0100

    GREEN

--- Blackjack/Model/Dealer.cs
+++ Blackjack/Model/Dealer.cs
@@ -44,7 +44,10 @@ namespace Blackjack.Model
             int[] cardValues = new int[(int)Value.Count]
                 {2, 3, 4, 5, 6, 7, 8, 9, 10, 10 ,10 ,10, 11};
             int score = 0;
-            score = cardValues[(int)Hand[0].GetValue()];
+            foreach (Card card in Hand)
+            {
+                score += cardValues[(int)card.GetValue()];
+            }
             return score;
         }
 
