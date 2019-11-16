418bc4d2e377db285826a612bfdf97fa8bef057a
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-16 08:13:15 +0100

    RED

---
+++ Blackjack/Model/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack.Model
+{
+    public class Player : IPlayer
+    {
+        public IReadOnlyList<ICard> Hand { get { return _hand; } }
+        private List<ICard> _hand = new List<ICard>();
+
+        public int GetCurrentScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCardToHand(ICard card)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlackjackTest/Model/PlayerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using Blackjack.Model;
+
+namespace BlackjackTest.Model
+{
+    public class PlayerTest
+    {
+        [Fact]
+        public void AddCardToHand_ShouldAddACardToPlayersHand()
+        {
+            IPlayer sut = new Player();
+            var mockCard = new Mock<ICard>(It.IsAny<Color>(), It.IsAny<Value>());
+            sut.AddCardToHand(mockCard.Object);
+            Assert.NotEmpty(sut.Hand);
+        }
+    }
+}
