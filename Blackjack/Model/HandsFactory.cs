ccca7ab5fce13b189bde1d71498ecbf7de43c847
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-16 15:47:31 +0100

    RED

---
+++ Blackjack/Model/HandsFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blackjack.Model
+{
+    public class HandsFactory
+    {
+        public IHands CreateNewHands(IReadOnlyList<ICard> playerCards, int playerScore, IReadOnlyList<ICard> dealerCards, int dealerScore)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlackjackTest/Model/HandsFactoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using Blackjack.Model;
+
+namespace BlackjackTest.Model
+{
+    public class HandsFactoryTest
+    {
+        [Fact]
+        public void CreateNewHands_ShouldReturnANewHandsObject()
+        {
+            HandsFactory sut = new HandsFactory();
+            Assert.IsType<Hands>(sut.CreateNewHands(It.IsAny<List<ICard>>(), It.IsAny<int>(), It.IsAny<List<ICard>>(), It.IsAny<int>()));
+        }
+    }
+}
