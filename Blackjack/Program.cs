dd8bf95162679e91d1130be81c585d4774c62db9
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-11-16 16:50:16 +0100

    Setup program

--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using Blackjack.Controller;
+using Blackjack.Model;
+using Blackjack.View;
 
 namespace Blackjack
 {
@@ -8,7 +11,16 @@ namespace Blackjack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            CardFactory cardFactory = new CardFactory();
+            Deck deck = new Deck(cardFactory, random);
+            Player player = new Player();
+            Dealer dealer = new Dealer(deck);
+            BlackjackGame game = new BlackjackGame(dealer,player);
+            GameView gameView = new GameView();
+            GameController gameController = new GameController(game, gameView);
+            MainController mainController = new MainController(gameController, gameView);
+            mainController.RunGame();
         }
     }
 }
